d5a933494147d697679e43a3b4d893a099c99864
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-02-28 20:31:58 +0530

    real time chat

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const { createServer } = require('node:http');
-// const { Server } = require("socket.io");
+const { Server } = require("socket.io");
 const PORT = 4001;
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    return res.send('./public/index.html');
+    res.send('./public/index.html');
 })
+
+// socket.io
+io.on('connection', (socket) => {
+    console.log('A new user connected', socket.id);
+    socket.on('disconnect', () =>{
+        console.log('User disconnected', socket.id);
+    })
+    socket.on('message', (message) => {
+        // console.log('Message is:', message);
+        io.emit('message', message);
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
