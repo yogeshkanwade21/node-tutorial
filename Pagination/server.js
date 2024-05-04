a2ffcd0f67134163ddb5a5545115a41f964ace3a
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-05-04 20:42:50 +0530

    next & prev checks

--- Pagination/server.js
+++ Pagination/server.js
@@ -26,12 +26,27 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-    const page = req.query.page;
-    const limit = req.query.limit;
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
 
     const startIndex = (page - 1) * 5;
-    const result = users.slice(startIndex, startIndex + limit);
-    res.send(result);
+    const endIndex = (page * limit);
+    const results = {};
+    if(endIndex < users.length){
+        results.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+    if(startIndex > 0){
+        results.prev = {
+            page: page - 1,
+            limit: limit
+        }
+    }
+
+    results.results = users.slice(startIndex, endIndex);
+    res.send(results);
 });
 
 app.listen(PORT, () => {
