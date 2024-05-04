ea2df2dcb52fe94487dce162af6397fb0a600743
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-05-04 21:23:30 +0530

    middleware for pagination

--- Pagination/server.js
+++ Pagination/server.js
@@ -17,36 +17,58 @@ const users = [
     { id: 11, name: 'User 11'},
     { id: 12, name: 'User 12'}
 ];
+const posts = [
+    { id: 1, name: 'Post 1'},
+    { id: 2, name: 'Post 2'},
+    { id: 3, name: 'Post 3'},
+    { id: 4, name: 'Post 4'},
+    { id: 5, name: 'Post 5'},
+    { id: 6, name: 'Post 6'},
+    { id: 7, name: 'Post 7'},
+    { id: 8, name: 'Post 8'},
+    { id: 9, name: 'Post 9'},
+    { id: 10, name: 'Post 10'},
+    { id: 11, name: 'Post 11'},
+    { id: 12, name: 'Post 12'}
+];
 
 //middleware
 app.use(express.json());
+function paginatedResults(model) {
+    return (req, res, next) => {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+    
+        const startIndex = (page - 1) * 5;
+        const endIndex = (page * limit);
+        const results = {};
+        if(endIndex < model.length){
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+        if(startIndex > 0){
+            results.prev = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+        results.results = model.slice(startIndex, endIndex);
+        res.paginatedResults = results;
+        next();
+    };
+}
 
 app.get('/', (req, res) => {
     res.send("Server running");
 });
 
-app.get('/users', (req, res) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-
-    const startIndex = (page - 1) * 5;
-    const endIndex = (page * limit);
-    const results = {};
-    if(endIndex < users.length){
-        results.next = {
-            page: page + 1,
-            limit: limit
-        }
-    }
-    if(startIndex > 0){
-        results.prev = {
-            page: page - 1,
-            limit: limit
-        }
-    }
-
-    results.results = users.slice(startIndex, endIndex);
-    res.send(results);
+app.get('/users', paginatedResults(users), (req, res) => {
+    res.send(res.paginatedResults);
+});
+app.get('/posts', paginatedResults(posts), (req, res) => {
+    res.send(res.paginatedResults);
 });
 
 app.listen(PORT, () => {
