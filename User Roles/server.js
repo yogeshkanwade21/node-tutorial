e62aa6df748831c8ee994bad005af20b1afe5645
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-05-05 12:48:57 +0530

    initial commit

---
+++ User Roles/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const { users } = require('./data')
+const projectRouter = require('./routes/project')
+const authUser = require('./basicAuth')
+
+app.use(express.json())
+app.use(setUser)
+app.use('/projects', projectRouter)
+
+app.get('/', (req, res) => {
+  res.send('Home Page')
+})
+
+app.get('/dashboard', authUser, (req, res) => {
+  res.send('Dashboard Page')
+})
+
+app.get('/admin', (req, res) => {
+  res.send('Admin Page')
+})
+
+function setUser(req, res, next) {
+  const userId = req.body.userId;
+  if (userId) {
+    req.user = users.find(user => user.id === userId);
+  }
+  next();
+}
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
